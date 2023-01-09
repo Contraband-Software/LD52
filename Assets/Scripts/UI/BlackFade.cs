4a0daf14b85d53a7b9c1e2714e6ad0d315ed94be
jakub bala <63164657+JakubBala@users.noreply.github.com>
2023-01-09 18:33:05 +0000

    [Feature]: Fade between scenes game

--- Assets/Scripts/UI/BlackFade.cs
+++ Assets/Scripts/UI/BlackFade.cs
@@ -6,8 +6,19 @@ using Architecture.Managers;
 
 public class BlackFade : MonoBehaviour
 {
+    
+    public enum WorkingInScene { MAINMENU, GAME}
+    public WorkingInScene scene;
+
     public void FadedToBlack()
     {
-        MainMenuController.GetReference().FadeInBlackCompleted();
+        if(scene == WorkingInScene.MAINMENU)
+        {
+            MainMenuController.GetReference().FadeInBlackCompleted();
+        }
+        if(scene == WorkingInScene.GAME)
+        {
+            UIControllerLevel.GetReference().FadeInBlackCompleted();
+        }
     }
 }
