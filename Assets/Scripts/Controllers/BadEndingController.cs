0a81cf8c07fd60cbeecc9c8dc1bf46b35280ef4d
jakub bala <63164657+JakubBala@users.noreply.github.com>
2023-01-09 17:32:11 +0000

    [Sync]

---
+++ Assets/Scripts/Controllers/BadEndingController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Architecture.Managers
+{
+    public class BadEndingController : MonoBehaviour
+    {
+        public void BackToMainMenu()
+        {
+            
+        }
+    }
+}
