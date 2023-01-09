e47a1e6047143567e6ac02c5c0702344f831c72a
jakub bala <63164657+JakubBala@users.noreply.github.com>
2023-01-09 17:02:58 +0000

    [Feature]: Background image changing logic

--- Assets/Scripts/Controllers/MainMenuController.cs
+++ Assets/Scripts/Controllers/MainMenuController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Architecture.Managers;
+using UnityEngine.UI;
 
 namespace Architecture.Managers
 {
@@ -16,6 +17,10 @@ namespace Architecture.Managers
         [SerializeField] GameObject continueButton;
         [SerializeField] CanvasGroup blackOverlay;
         [SerializeField] Animator blackOverlayAnim;
+        [SerializeField] Image backgroundImage;
+
+        [Header("Other referecnes")]
+        [SerializeField] Sprite foggyBackground;
 
 
         // Start is called before the first frame update
@@ -28,6 +33,10 @@ namespace Architecture.Managers
             {
                 continueButton.SetActive(false);
             }
+            else
+            {
+                backgroundImage.sprite = foggyBackground;
+            }
 
             blackOverlay.blocksRaycasts = false;
             blackOverlay.alpha = 0f;
@@ -36,21 +45,24 @@ namespace Architecture.Managers
 
         public void ContinueGame()
         {
+            FadeInBlackOverlay();
+
             //Load next level, disable scene activation
             GameController.Instance.ContinueGame();
             GameController.Instance.SceneLoadingOperation.allowSceneActivation = false;
-
-            //
-            blackOverlay.blocksRaycasts = true;
-            blackOverlayAnim.Play("FadeInBlack");
         }
 
         public void NewGame()
         {
+            FadeInBlackOverlay();
+
             //Load next level, disable scene activation
             GameController.Instance.StartNewGame();
             GameController.Instance.SceneLoadingOperation.allowSceneActivation = false;
-            //
+        }
+
+        private void FadeInBlackOverlay()
+        {
             blackOverlay.blocksRaycasts = true;
             blackOverlayAnim.Play("FadeInBlack");
         }
